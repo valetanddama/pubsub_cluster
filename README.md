340ae1176c70b4b4b0dd41e5d6cf4cda9f115811
alukinykh <valetanddama@gmail.com>
2018-10-18 21:12:25 +0500

    init

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ go test -benchmem -bench=. test/benchmark_test.go
 
 BenchmarkSendMessageForRoleGenerator-8   30000         51225  ns/op     240 B/op     7  allocs/op
 BenchmarkGetMessageForRoleHandler-8      20000         55303  ns/op     516 B/op     15 allocs/op
-BenchmarkCheckServerRole-8               10000         150301 ns/op     656 B/op     20 allocs/op
+BenchmarkCheckServerRole-8               20000         93275  ns/op     392 B/op     13 allocs/op
 BenchmarkGetRedisConn-8                  1000000000    2.56   ns/op     0   B/op     0  allocs/op
 BenchmarkPing-8                          30000         48672  ns/op     148 B/op     5  allocs/op
 ```
--- pkg/services/server_handler.go
+++ pkg/services/server_handler.go
@@ -1,27 +1,34 @@
 package services
 
 import (
+	"github.com/go-redis/redis"
 	"github.com/valetanddama/cluster/pkg/config"
 	"log"
 	"time"
 )
 
 func CheckServerRole() {
-	result, err := config.Conn.Redis().SetNX(config.KeyPublisher, config.Conn.ServerID, 2*time.Second).Result()
+	serverID, err := config.Conn.Redis().Get(config.KeyPublisher).Result()
 
-	if err != nil {
+	if err != nil && err != redis.Nil {
 		log.Println(err)
 		return
 	}
 
-	if result == false {
-		serverID, err := config.Conn.Redis().Get(config.KeyPublisher).Result()
+	if serverID == "" {
+		result, err := config.Conn.Redis().SetNX(config.KeyPublisher, config.Conn.ServerID, 2*time.Second).Result()
 
 		if err != nil {
 			log.Println(err)
 			return
 		}
 
+		if result == true {
+			config.Conn.ServerRole = config.RoleGeneratorMessages
+		} else {
+			config.Conn.ServerRole = config.RoleHandlerMessages
+		}
+	} else {
 		if serverID == config.Conn.ServerID {
 			if err := config.Conn.Redis().Expire(config.KeyPublisher, 2*time.Second).Err(); err != nil {
 				log.Println(err)
@@ -32,7 +39,5 @@ func CheckServerRole() {
 		} else {
 			config.Conn.ServerRole = config.RoleHandlerMessages
 		}
-	} else {
-		config.Conn.ServerRole = config.RoleGeneratorMessages
 	}
 }
